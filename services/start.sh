2f6818821a85f57130e11e4dbb503903a4b60d7a
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-01-12 18:15:08 -0300

    Removing Claude model

--- services/start.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    --name litellm \
-    -v $(pwd)/litellm_config.yaml:/app/config.yaml \
-    -e GOOGLE_AI_API_KEY="$(echo $GOOGLE_AI_API_KEY)" \
-    -p 4000:4000 \
-    ghcr.io/berriai/litellm:main-v1.57.1 \
-    --config /app/config.yaml --detailed_debug \
